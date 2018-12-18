817a7b543d741ed5e548949f38c72aea3f35fcc3
George Kaplan <george.kaplan@visionistinc.com>
2018-12-18 15:39:44 -0500

    minimal working scheme reader

---
+++ src/main.rs
@@ -0,0 +1,139 @@
+extern crate failure_derive;
+extern crate quicli;
+extern crate structopt;
+
+use std::str::FromStr;
+
+use quicli::prelude::*;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Cli {
+    file: String,
+    #[structopt(flatten)]
+    verbosity: Verbosity,
+}
+
+#[derive(Debug, Fail)]
+enum LispError {
+    #[fail(display = "could not parse expression: {}", exp)]
+    SyntaxError { exp: String },
+}
+
+fn is_atom_char(c: char) -> bool {
+    c.is_alphanumeric() || c == '-' || c == '_' || c == '*'
+}
+
+fn find_closing_paren(s: &str) -> Option<usize> {
+    let mut depth = 0;
+
+    for (idx, c) in s.chars().enumerate() {
+        match c {
+            '(' => depth += 1,
+            ')' => depth -= 1,
+            _ => (),
+        }
+
+        if depth == 0 {
+            return Some(idx);
+        }
+    }
+
+    None
+}
+
+#[derive(Debug)]
+enum SExp {
+    Atom(String),
+    List(Vec<SExp>),
+}
+
+impl FromStr for SExp {
+    type Err = LispError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let code = s.trim().to_owned();
+
+        if code.chars().all(is_atom_char) {
+            debug!("Matched atom: {}", code);
+            Ok(SExp::Atom(code))
+        } else if code.starts_with("(") && code.ends_with(")") {
+            match find_closing_paren(&code) {
+                Some(idx) => {
+                    debug!("Matched list with length {} chars", idx + 1);
+                    let mut list_str = code.clone();
+                    let mut list_out: Vec<SExp> = Vec::new();
+
+                    if idx + 1 == code.len() {
+                        list_str = code.get(1..idx).unwrap().to_string();
+                    }
+
+                    while !list_str.is_empty() {
+                        debug!(
+                            "Processing list string with length {} chars",
+                            list_str.len()
+                        );
+
+                        if list_str.starts_with("(") {
+                            match find_closing_paren(&list_str) {
+                                Some(idx2) => {
+                                    if idx2 + 1 == list_str.len() {
+                                        debug!("Whole string is a single list");
+                                        list_out.push(list_str.parse::<SExp>()?);
+                                        break;
+                                    } else {
+                                        debug!("Matched sub-list with length {} chars", idx2 + 1);
+                                        let (before, after) = list_str.split_at(idx2 + 1);
+                                        list_out.push(before.parse::<SExp>()?);
+                                        list_str = after.trim().to_string();
+                                    }
+                                }
+                                None => {
+                                    return Err(LispError::SyntaxError {
+                                        exp: list_str.to_string(),
+                                    });
+                                }
+                            }
+                        } else {
+                            match list_str.find(|c| !is_atom_char(c)) {
+                                Some(idx3) => {
+                                    debug!(
+                                        "Matched atom in first position with length {} chars",
+                                        idx3 + 1
+                                    );
+                                    let (first, rest) = list_str.split_at(idx3);
+                                    list_out.push(first.parse::<SExp>()?);
+                                    list_str = rest.trim().to_string();
+                                }
+                                None => {
+                                    list_out.push(list_str.parse::<SExp>()?);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    Ok(SExp::List(list_out))
+                }
+                None => Err(LispError::SyntaxError { exp: code }),
+            }
+        } else {
+            Err(LispError::SyntaxError { exp: code })
+        }
+    }
+}
+
+fn main() -> CliResult {
+    let args = Cli::from_args();
+    args.verbosity.setup_env_logger("rsch")?;
+
+    info!("Reading source from {}", args.file);
+
+    let code = read_file(&args.file)?;
+    match code.parse::<SExp>() {
+        Ok(tree) => println!("{:?}", tree),
+        Err(error) => error!("{}", error),
+    };
+
+    Ok(())
+}
