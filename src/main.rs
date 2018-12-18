a2f9427a424ee35f1965e08868760cb1f7d499fd
George Kaplan <george.kaplan@visionistinc.com>
2018-12-18 17:40:29 -0500

    added tests, fixed issues that they revealed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(const_vec_new)]
+
 extern crate failure_derive;
 extern crate quicli;
 extern crate structopt;
@@ -6,6 +8,9 @@ use std::str::FromStr;
 
 use quicli::prelude::*;
 use structopt::StructOpt;
+
+#[allow(dead_code)]
+const NULL: SExp = SExp::List(Vec::new());
 
 #[derive(Debug, StructOpt)]
 struct Cli {
@@ -24,14 +29,24 @@ fn is_atom_char(c: char) -> bool {
     !c.is_whitespace() && !c.is_control() && c != '(' && c != ')'
 }
 
-fn find_closing_paren(s: &str) -> Option<usize> {
+fn is_symbol_char(c: char) -> bool {
+    is_atom_char(c) && (c.is_alphanumeric() || c == '-' || c == '_' || c == '?' || c == '*')
+}
+
+fn find_closing_delim(s: &str, d_plus: char, d_minus: char) -> Option<usize> {
     let mut depth = 0;
 
     for (idx, c) in s.chars().enumerate() {
-        match c {
-            '(' => depth += 1,
-            ')' => depth -= 1,
-            _ => (),
+        if d_plus == d_minus {
+            if c == d_plus {
+                depth = !depth;
+            }
+        } else {
+            match c {
+                x if x == d_plus => depth += 1,
+                x if x == d_minus => depth -= 1,
+                _ => (),
+            }
         }
 
         if depth == 0 {
@@ -42,7 +57,7 @@ fn find_closing_paren(s: &str) -> Option<usize> {
     None
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Primitive {
     Boolean(bool),
     Character(char),
@@ -67,17 +82,20 @@ impl FromStr for Primitive {
             _ => (),
         }
 
-        if s.len() == 1 {
-            return Ok(Primitive::Character(s.chars().next().unwrap()));
+        if s.len() == 3 && s.starts_with("'") && s.ends_with("'") {
+            return Ok(Primitive::Character(s.chars().skip(1).next().unwrap()));
         }
 
         if s.starts_with('"') && s.ends_with('"') {
-            return Ok(Primitive::String(
-                s.get(1..(s.len() - 1)).unwrap().to_string(),
-            ));
+            match find_closing_delim(s, '"', '"') {
+                Some(idx) if idx + 1 == s.len() => {
+                    return Ok(Primitive::String(s.get(1..idx).unwrap().to_string()));
+                }
+                _ => (),
+            }
         }
 
-        if s.chars().all(is_atom_char) {
+        if s.chars().all(is_symbol_char) {
             return Ok(Primitive::Symbol(s.to_string()));
         }
 
@@ -85,7 +103,7 @@ impl FromStr for Primitive {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum SExp {
     Atom(Primitive),
     List(Vec<SExp>),
@@ -100,8 +118,8 @@ impl FromStr for SExp {
         if code.chars().all(is_atom_char) {
             debug!("Matched atom: {}", code);
             Ok(SExp::Atom(code.parse::<Primitive>()?))
-        } else if code.starts_with("(") && code.ends_with(")") {
-            match find_closing_paren(&code) {
+        } else if code.starts_with('(') && code.ends_with(')') {
+            match find_closing_delim(&code, '(', ')') {
                 Some(idx) => {
                     debug!("Matched list with length {} chars", idx + 1);
                     let mut list_str = code.clone();
@@ -117,8 +135,8 @@ impl FromStr for SExp {
                             list_str.len()
                         );
 
-                        if list_str.starts_with("(") {
-                            match find_closing_paren(&list_str) {
+                        if list_str.starts_with('(') {
+                            match find_closing_delim(&list_str, '(', ')') {
                                 Some(idx2) => {
                                     if idx2 + 1 == list_str.len() {
                                         debug!("Whole string is a single list");
@@ -138,6 +156,11 @@ impl FromStr for SExp {
                                 }
                             }
                         } else {
+                            if let Ok(prim_val) = list_str.parse::<Primitive>() {
+                                list_out.push(SExp::Atom(prim_val));
+                                break;
+                            }
+
                             match list_str.find(|c| !is_atom_char(c)) {
                                 Some(idx3) => {
                                     debug!(
@@ -161,7 +184,8 @@ impl FromStr for SExp {
                 None => Err(LispError::SyntaxError { exp: code }),
             }
         } else {
-            Err(LispError::SyntaxError { exp: code })
+            let prim = code.parse::<Primitive>()?;
+            Ok(SExp::Atom(prim))
         }
     }
 }
@@ -179,4 +203,84 @@ fn main() -> CliResult {
     };
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SExp::{Atom, List};
+    use super::*;
+
+    fn do_parse_and_assert(test_val: &str, expected_val: SExp) {
+        let test_parsed = test_val.parse::<SExp>().unwrap();
+        assert_eq!(test_parsed, expected_val);
+    }
+
+    fn mk_sym(s: &str) -> SExp {
+        Atom(Primitive::Symbol(s.to_string()))
+    }
+
+    #[test]
+    fn parse_empty_list() {
+        do_parse_and_assert("()", NULL);
+    }
+
+    #[test]
+    fn parse_list_of_lists() {
+        do_parse_and_assert("(() () ())", List(vec![NULL, NULL, NULL]));
+    }
+
+    #[test]
+    fn parse_atom() {
+        do_parse_and_assert("hello", mk_sym("hello"));
+    }
+
+    #[test]
+    fn parse_list_of_atoms() {
+        do_parse_and_assert(
+            "(a bc de fgh ijk l mnop)",
+            List(vec![
+                mk_sym("a"),
+                mk_sym("bc"),
+                mk_sym("de"),
+                mk_sym("fgh"),
+                mk_sym("ijk"),
+                mk_sym("l"),
+                mk_sym("mnop"),
+            ]),
+        );
+    }
+
+    #[test]
+    fn parse_primitive_types() {
+        do_parse_and_assert("#f", Atom(Primitive::Boolean(false)));
+        do_parse_and_assert("#t", Atom(Primitive::Boolean(true)));
+        do_parse_and_assert("0", Atom(Primitive::Number(0_f64)));
+        do_parse_and_assert("2.0", Atom(Primitive::Number(2.0)));
+        do_parse_and_assert("inf", Atom(Primitive::Number(std::f64::INFINITY)));
+        do_parse_and_assert("-inf", Atom(Primitive::Number(std::f64::NEG_INFINITY)));
+        do_parse_and_assert("'c'", Atom(Primitive::Character('c')));
+        do_parse_and_assert("'''", Atom(Primitive::Character('\'')));
+        do_parse_and_assert(
+            r#""test string with spaces""#,
+            Atom(Primitive::String("test string with spaces".to_string())),
+        );
+    }
+
+    #[test]
+    fn parse_mixed_type_list() {
+        do_parse_and_assert(
+            "(0 #f () 33.5 \"xyz\" '?' #t \"\" \"   \")",
+            List(vec![
+                Atom(Primitive::Number(0_f64)),
+                Atom(Primitive::Boolean(false)),
+                NULL,
+                Atom(Primitive::Number(33.5)),
+                Atom(Primitive::String("xyz".to_string())),
+                Atom(Primitive::Character('?')),
+                Atom(Primitive::Boolean(true)),
+                Atom(Primitive::String("".to_string())),
+                Atom(Primitive::String("   ".to_string())),
+            ]),
+        );
+    }
 }
