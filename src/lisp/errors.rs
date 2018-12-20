7e4e719396aed5d1b0a73d24a51d3a411681dd93
George Kaplan <george@georgekaplan.xyz>
2018-12-20 02:30:20 -0500

    restructured repo a bit

---
+++ src/lisp/errors.rs
@@ -0,0 +1,18 @@
+#[derive(Debug, Fail)]
+pub enum LispError {
+    #[fail(display = "could not parse expression: {}", exp)]
+    SyntaxError { exp: String },
+    #[fail(display = "symbol is not defined: {}", sym)]
+    UndefinedSymbol { sym: String },
+    #[fail(
+        display = "too many arguments provided: expected {}, got {}.",
+        right_num, n_args
+    )]
+    TooManyArguments { n_args: usize, right_num: usize },
+    #[fail(display = "{} expects at least one argument.", symbol)]
+    NoArgumentsProvided { symbol: String },
+    #[fail(display = "Expected a list, got {}.", atom)]
+    NotAList { atom: String },
+    #[fail(display = "Expected a pair, got the null list.")]
+    NullList,
+}
