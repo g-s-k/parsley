a243ec8b538ce724455fbd95d62a2cf5ce10f670
George Kaplan <george@georgekaplan.xyz>
2018-12-20 06:10:40 -0500

    optional read from stdin

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ extern crate failure_derive;
 extern crate quicli;
 extern crate structopt;
 
+use std::io::{self, Read};
+use std::path::PathBuf;
+
 use quicli::prelude::*;
 use structopt::StructOpt;
 
@@ -13,7 +16,7 @@ use self::lisp::{Context, SExp};
 
 #[derive(Debug, StructOpt)]
 struct Cli {
-    file: String,
+    file: Option<PathBuf>,
     #[structopt(flatten)]
     verbosity: Verbosity,
 }
@@ -22,10 +25,18 @@ fn main() -> CliResult {
     let args = Cli::from_args();
     args.verbosity.setup_env_logger("rsch")?;
 
+    info!("Creating base namespace.");
     let mut base_context = Context::base();
 
-    info!("Reading source from {}", args.file);
-    let code = read_file(&args.file)?;
+
+    let code = if let Some(f_name) = args.file {
+        info!("Reading source from {:?}", f_name);
+        read_file(&f_name)?
+    } else {
+        let mut code_buffer = String::new();
+        io::stdin().read_to_string(&mut code_buffer)?;
+        code_buffer
+    };
 
     info!("Parsing source code.");
     match code.parse::<SExp>() {
