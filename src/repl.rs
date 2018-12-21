91ba79a95e49e5378db488193a5d2f1ae5452698
George Kaplan <george@georgekaplan.xyz>
2018-12-21 06:34:06 -0500

    use raw string for repl prompt

--- src/repl.rs
+++ src/repl.rs
@@ -5,21 +5,23 @@ use quicli::prelude::*;
 use parsley::{self, Context};
 
 const REPL_PROMPT: &str = "> ";
+const REPL_WELCOME: &str = r#"
+'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()
+'()          Welcome to PARSLEY, an interactive Scheme interpreter.        '()
+'()          You are using version 0.1.0.                                  '()
+'()          Press <CTRL>-C or enter `.exit` to quit.                      '()
+'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()
+
+"#;
 
 pub fn repl(ctx: &mut Context) -> io::Result<usize> {
     info!("Initializing REPL.");
 
-    println!();
-    println!("'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()");
-    println!("'()          Welcome to PARSLEY, an interactive Scheme interpreter.        '()");
-    println!("'()          You are using version 0.1.0.                                  '()");
-    println!("'()          Press <CTRL>-C or enter `.exit` to quit.                      '()");
-    println!("'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()'()");
-    println!();
-
     let mut buffer = String::new();
 
+    io::stdout().write_all(REPL_WELCOME.as_bytes())?;
     io::stdout().flush()?;
+
     loop {
         // write prompt and ensure it actually prints
         io::stdout().write_all(REPL_PROMPT.as_bytes())?;
