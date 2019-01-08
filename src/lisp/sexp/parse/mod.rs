f8d4078243b05059f7b8a6c128e4d77855c485fe
George Kaplan <george@georgekaplan.xyz>
2019-01-07 22:02:21 -0500

    reduced cyclomatic complexity in parse module

--- src/lisp/sexp/parse/mod.rs
+++ src/lisp/sexp/parse/mod.rs
@@ -48,83 +48,7 @@ impl SExp {
                 Some(idx) if idx == 1 => Ok(Null),
                 Some(idx) => {
                     debug!("Matched list with length {} chars", idx + 1);
-                    let mut list_str = code[1..idx].trim();
-                    let mut list_out = Null;
-
-                    while !list_str.is_empty() {
-                        debug!(
-                            "Processing list string with length {} chars",
-                            list_str.len()
-                        );
-
-                        if list_str.ends_with(')') {
-                            match utils::find_closing_delim(list_str.chars().rev(), ')', '(') {
-                                None => {
-                                    return Err(LispError::SyntaxError {
-                                        exp: list_str.to_string(),
-                                    });
-                                }
-                                Some(idx2) => {
-                                    debug!("Matched sub-list with length {} chars", idx2 + 1);
-                                    let mut new_idx = list_str.len() - 1 - idx2;
-                                    if new_idx > 0 {
-                                        if let Some('\'') = list_str.chars().nth(new_idx - 1) {
-                                            new_idx -= 1;
-                                        }
-                                    }
-
-                                    let (before, after) = list_str.split_at(new_idx);
-                                    list_str = before.trim();
-                                    list_out = Pair {
-                                        head: Box::new(SExp::parse_str(after)?),
-                                        tail: Box::new(list_out),
-                                    };
-                                }
-                            }
-                        } else {
-                            if list_str.ends_with('"') {
-                                match list_str.chars().rev().skip(1).position(|e| e == '"') {
-                                    Some(idx2) => {
-                                        debug!("Matched string with length {} chars", idx2);
-                                        let (rest, last) =
-                                            list_str.split_at(list_str.len() - 2 - idx2);
-                                        list_out = list_out.cons(Atom(last.parse::<Primitive>()?));
-                                        list_str = rest.trim();
-                                    }
-                                    None => {
-                                        return Err(LispError::SyntaxError {
-                                            exp: list_str.to_string(),
-                                        });
-                                    }
-                                }
-                            }
-
-                            match list_str.chars().rev().position(|c| !utils::is_atom_char(c)) {
-                                Some(idx3) => {
-                                    debug!(
-                                        "Matched atom in first position with length {} chars",
-                                        idx3
-                                    );
-                                    let (rest, last) = list_str.split_at(list_str.len() - idx3);
-                                    list_out = Pair {
-                                        head: Box::new(SExp::parse_str(last)?),
-                                        tail: Box::new(list_out),
-                                    };
-                                    list_str = rest.trim();
-                                }
-                                _ => {
-                                    debug!("Entire string is an atom.");
-                                    list_out = Pair {
-                                        head: Box::new(SExp::parse_str(list_str)?),
-                                        tail: Box::new(list_out),
-                                    };
-                                    break;
-                                }
-                            }
-                        }
-                    }
-
-                    Ok(list_out)
+                    SExp::parse_list_from_str(&code[1..idx])
                 }
                 None => Err(LispError::SyntaxError {
                     exp: code.to_string(),
@@ -134,5 +58,85 @@ impl SExp {
             let prim = code.parse::<Primitive>()?;
             Ok(Atom(prim))
         }
+    }
+
+    fn parse_list_from_str(s: &str) -> LispResult {
+        let mut list_str = s.trim();
+        let mut list_out = Null;
+
+        while !list_str.is_empty() {
+            debug!(
+                "Processing list string with length {} chars",
+                list_str.len()
+            );
+
+            if list_str.ends_with(')') {
+                match utils::find_closing_delim(list_str.chars().rev(), ')', '(') {
+                    None => {
+                        return Err(LispError::SyntaxError {
+                            exp: list_str.to_string(),
+                        });
+                    }
+                    Some(idx2) => {
+                        debug!("Matched sub-list with length {} chars", idx2 + 1);
+                        let mut new_idx = list_str.len() - 1 - idx2;
+                        if new_idx > 0 {
+                            while let Some('\'') = list_str.chars().nth(new_idx - 1) {
+                                new_idx -= 1;
+                            }
+                        }
+
+                        let (before, after) = list_str.split_at(new_idx);
+                        list_str = before.trim();
+                        list_out = Pair {
+                            head: Box::new(SExp::parse_str(after)?),
+                            tail: Box::new(list_out),
+                        };
+                    }
+                }
+            } else {
+                if list_str.ends_with('"') {
+                    match list_str.chars().rev().skip(1).position(|e| e == '"') {
+                        Some(idx2) => {
+                            debug!("Matched string with length {} chars", idx2);
+                            let (rest, last) =
+                                list_str.split_at(list_str.len() - 2 - idx2);
+                            list_out = list_out.cons(Atom(last.parse::<Primitive>()?));
+                            list_str = rest.trim();
+                        }
+                        None => {
+                            return Err(LispError::SyntaxError {
+                                exp: list_str.to_string(),
+                            });
+                        }
+                    }
+                }
+
+                match list_str.chars().rev().position(|c| !utils::is_atom_char(c)) {
+                    Some(idx3) => {
+                        debug!(
+                            "Matched atom in first position with length {} chars",
+                            idx3
+                        );
+                        let (rest, last) = list_str.split_at(list_str.len() - idx3);
+                        list_out = Pair {
+                            head: Box::new(SExp::parse_str(last)?),
+                            tail: Box::new(list_out),
+                        };
+                        list_str = rest.trim();
+                    }
+                    _ => {
+                        debug!("Entire string is an atom.");
+                        list_out = Pair {
+                            head: Box::new(SExp::parse_str(list_str)?),
+                            tail: Box::new(list_out),
+                        };
+                        break;
+                    }
+                }
+            }
+        }
+
+        Ok(list_out)
     }
 }
